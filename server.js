6abd0a0df96084e8e7cadde61a83b7a74775ca97
andrea <andrea_coronado20212@elpoli.edu.co>
2025-11-18 14:56:42 -0500

    Ajuste de puerto para Render

--- server.js
+++ server.js
@@ -75,4 +75,6 @@ app.get("/api/xml", (req, res) => {
     res.send(xml);
 });
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+// ⭐ PORT para Render (IMPORTANTE)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor corriendo en puerto: " + PORT));
